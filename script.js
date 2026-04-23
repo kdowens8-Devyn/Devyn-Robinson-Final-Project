8d628303a50bd57ed8835b8d71e42dff91445f72
kdowens8 <71532072+kdowens8-Devyn@users.noreply.github.com>
2026-04-23 14:18:18 -0400

    Dark mode testing
    Dark mode wasn't working so I added the event listener to the beginning to try and fix the problem i ran into. This should enable to button to work as intended.
    
    I also added to the css so i can see the button and text

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", function (){
 const toggleBtn = document.getElementById("themeToggle");
 
-toggleBtn.addEventListener("click", function () {
+    toggleBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
-})
+    })
+
+});
